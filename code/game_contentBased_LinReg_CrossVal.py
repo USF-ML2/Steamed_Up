b16beea77fcd4737f674edb75e0ddfe37162e0d5
Paul Thompson <paulthompson@Pauls-MacBook-Pro.local>
2016-03-05 19:03:13 -0800

    Updates: *Includes function for linear regression. *Features based on best of latest experimentation - likely to change.

--- code/game_contentBased_LinReg_CrossVal.py
+++ code/game_contentBased_LinReg_CrossVal.py
@@ -4,19 +4,20 @@ from sklearn import linear_model
 from scipy import sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.sparse import hstack
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
 game_path = "/Users/paulthompson/Documents/MSAN_Files/Spr1_AdvML/Final_Proj/gameData.txt"
 user_path = "/Users/paulthompson/Documents/MSAN_Files/Spr1_AdvML/Final_Proj/userData.txt"
 appID_path = "/Users/paulthompson/Documents/MSAN_Files/Spr1_AdvML/Final_Proj/appIDs.json"
 
-
 def create_game_profile_df(game_data_path):
     print "Creating Game Profiles"
     with open(game_data_path) as f:
-        gameFeatDicts = []
-        gameCount = 0
-        gameNameDict = {}
-        gameNameList = []
+        #Initialization
+        gameFeatDicts = []; gameCount = 0; gameNameDict = {}; gameNameList = []; gameDescDict = {}; gameDescList = []
+
+        #Game File Feature Extraction
         for line in f:
             record = json.loads(line)
             if 'data' in record['details'][record['details'].keys()[0]].keys():
@@ -28,6 +29,7 @@ def create_game_profile_df(game_data_path):
                     # print record['details'][record['details'].keys()[0]]['data']
                 gameFeatDict['steam_appid'] = record['appid']
                 gameNameDict[record['appid']] = record['name']
+                gameDescDict[record['appid']] = record['details'][record['details'].keys()[0]]['data']['detailed_description']
                 # except:
                 #     pass
                 try:
@@ -112,14 +114,40 @@ def create_game_profile_df(game_data_path):
         gameFeaturesDF.index = gameFeaturesDF['steam_appid']
         for id in gameFeaturesDF.index:
             gameNameList.append(gameNameDict[id])
+            gameDescList.append(gameDescDict[id])
 
-        vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 1.0, lowercase=False,
-                                 stop_words = 'english', ngram_range = (1,2), analyzer='word', norm = 'l1')
-        X_train_TFIDFNames = vectorizer.fit_transform(gameNameList)
+        #Game Description LDA Mapping
+        n_features = 2000
+        n_topics = 20
+        n_top_words = 20
+        tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=n_features, stop_words='english')
+        tf = tf_vectorizer.fit_transform(gameDescList)
+        lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
+                                    learning_method='online', learning_offset=50.,
+                                    random_state=0)
+        gameDescTopics = sparse.coo_matrix(lda.fit_transform(tf))
+
+        #Game Name TFIDF
+        # tfidf_vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 1.0, lowercase=False,
+        #                          stop_words = 'english', ngram_range = (1,1), analyzer='word', norm = 'l1')
+        # X_train_TFIDFNames = tfidf_vectorizer.fit_transform(gameNameList)
+
+        #Game Desc TFIDF
+        # tfidf_vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 1.0, lowercase=False,
+        #                          stop_words = 'english', ngram_range = (1,1), analyzer='word', norm = 'l1')
+        # X_train_TFIDFNames = tfidf_vectorizer.fit_transform(gameDescList)
+
+        #Y and X Matrix Transformations
         gameFeaturesDF = gameFeaturesDF.drop(['steam_appid'], axis=1)
         Y_train = pandas.Series(np.zeros(len(gameFeaturesDF)), index = gameFeaturesDF.index)
         X_train_sparse = sparse.coo_matrix(gameFeaturesDF)
-        X_train_sparse2 = hstack([X_train_sparse, X_train_TFIDFNames])
+
+        ###Different versions of X_train_sparse2 just for experimenting
+        # X_train_sparse2 = hstack([X_train_sparse, X_train_TFIDFNames, gameDescTopics])
+        # X_train_sparse2 = hstack([X_train_sparse, X_train_TFIDFNames])
+        X_train_sparse2 = hstack([X_train_sparse, gameDescTopics])
+        # X_train_sparse2 = X_train_sparse
+
         X_train = pandas.DataFrame(X_train_sparse2.todense(), index=gameFeaturesDF.index)
         print "Finished Creating Game Profiles"
         return Y_train, X_train_sparse2, X_train
@@ -178,6 +206,7 @@ def CrossValUsingLinReg(userGames, Y_train, X_train, X_train_sparse):
         IDindexDict[id] = i
 
     print "Linear Regression Cross-Validation"
+    print ""
     for i in range(gameCount):
         # "Splitting Out Training and Test Y and X"
         tempAppIDs = copy.copy(appIDs)
@@ -190,8 +219,9 @@ def CrossValUsingLinReg(userGames, Y_train, X_train, X_train_sparse):
         X_test_sparse = sparse.coo_matrix(X_test)
 
         # "Fitting Linear Model"
+        # regr = linear_model.LinearRegression()
         regr = linear_model.LinearRegression()
-        regr.fit(X_train_sparse,Y_train)
+        regr.fit(X_train_sparse, Y_temp_train)
 
         # "Getting Predictions"
         predictions = list(regr.predict(X_test_sparse))
@@ -205,7 +235,7 @@ def CrossValUsingLinReg(userGames, Y_train, X_train, X_train_sparse):
         if appIDs[i] in np.array(topRecommendations)[:,0]:
             print "Success"
             recAccuracyCount += 1
-            print recAccuracyCount
+            print recAccuracyCount, "out of", i, "given total of", gameCount
             print ""
         else:
             print "Failure"
@@ -217,25 +247,55 @@ def CrossValUsingLinReg(userGames, Y_train, X_train, X_train_sparse):
     print ""
     print recAccuracyCount, "of the user's games out of", gameCount, "recommended."
 
+def returnTopLinRegRecs(userGames, Y_train, X_train, X_train_sparse, nRecs = 30):
+    IDindexDict = {}
+    appIDs = []
+    ErrorList = [2430]
+    for game in userGames:
+        if game['appid'] not in ErrorList:
+            try:
+                Y_train[game['appid']] = game['playtime_forever']
+                appIDs.append(game['appid'])
+            except:
+                pass
+
+    for i, id in enumerate(Y_train.index):
+        IDindexDict[id] = i
+
+    print "Running Linear Regression"
+    print ""
+
+    X_test = X_train.drop(appIDs, axis=0)
+    unPlayedGames = list(X_test.index)
+    X_test_sparse = sparse.coo_matrix(X_test)
+    regr = linear_model.LinearRegression()
+    regr.fit(X_train_sparse, Y_train)
+    predictions = list(regr.predict(X_test_sparse))
+    gamePredictions = []
+    for k in range(len(unPlayedGames)):
+        gamePredictions.append([unPlayedGames[k], predictions[k]])
+    topRecommendations = sorted(gamePredictions, key= lambda x: x[1], reverse = True)[0:nRecs]
+
+    return topRecommendations
+
+
 if __name__ == '__main__':
     print "Getting User Games"
     userIDList, userGamesList = get_user_games(user_path, numToRetrieve = 2)
     PlayTimeZeros, GameDF_sparse, GameDF = create_game_profile_df(game_path)
     selectedUserGames = userGamesList[userIDList[0]]
-    CrossValUsingLinReg(selectedUserGames, PlayTimeZeros, GameDF, GameDF_sparse)
 
-    # IDNameDict = getAppNames()
-    # count = 0
-    # print "Users Current Game List:"
-    # for game in selectedUserGames:
-    #     print game
-    # print ""
-    # print "Recommended Games List"
-    # for app in appIDRankings:
-    #     print IDNameDict[str(int(app[0]))]
-    #     count += 1
-    #     if count == 20:
-    #         sys.exit()
+    ### Cross Val for testing
+    # CrossValUsingLinReg(selectedUserGames, PlayTimeZeros, GameDF, GameDF_sparse)
+
+    ### Return Single Recommendation list
+    IDNameDict = getAppNames()
+    topRecs = returnTopLinRegRecs(userGames=selectedUserGames, Y_train=PlayTimeZeros, X_train=GameDF,
+                                  X_train_sparse=GameDF_sparse, nRecs = 30)
+    topRecNames = []
+    for id in np.array(topRecs)[:,0]:
+        topRecNames.append(IDNameDict[str(int(id))])
+    print topRecNames
 
 
 
